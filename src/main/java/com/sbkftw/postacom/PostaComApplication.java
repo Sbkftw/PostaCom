0bf80da191ab12e71123004f564b31fbcbe24e8d
jriquette <jriquette@cirb.brussels>
2020-12-18 15:35:48 +0100

    Initial commit

---
+++ src/main/java/com/sbkftw/postacom/PostaComApplication.java
@@ -0,0 +1,13 @@
+package com.sbkftw.postacom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostaComApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostaComApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sbkftw/postacom/PostaComApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbkftw.postacom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostaComApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
